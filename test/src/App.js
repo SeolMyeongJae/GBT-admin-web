7f5d557cc65627e9f762c429cecabc9431f3c4a9
wdwgrg <93629595+wdwgrg@users.noreply.github.com1~>
2022-03-15 15:54:37 +0900

    test

--- test/src/App.js
+++ test/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import TodoApp from './TodoApp';
 function App() {
     return (
-        <div className='App'>
+        <div className="App">
             <TodoApp />
-            {/* test */}
+            {/* test2 */}
         </div>
     );
 }
